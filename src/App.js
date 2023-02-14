0ee0a7faf12d7bedeaaa1a807bbcb714b0616558
Gaurav <joshi.gaurav171@gmail>
2023-02-13 23:00:12 -0800

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+let name = "Gaurav";
+function App() {
+ return(
+    <>
+ <div className="container">
+    <h1>Hello {name}</h1>
+ </div>
+ </>
+ )
+}
+
+export default App;
